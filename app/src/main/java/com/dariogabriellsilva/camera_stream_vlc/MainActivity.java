23e9af8f149ce9c8086a46529135846656a1f66c
Dario <dariogabriel113@gmail.com>
2019-10-07 11:03:22 -0300

    Initial commit

---
+++ app/src/main/java/com/dariogabriellsilva/camera_stream_vlc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dariogabriellsilva.camera_stream_vlc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
